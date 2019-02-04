1e05e7a15ef06117f067fcdbb7d74985eaa663a6
Lidii <lidiiany92@gmail.com>
2019-02-04 14:58:51 -0500

    Added unprotected routes

---
+++ routes/plantsUprotected.js
@@ -0,0 +1,61 @@
+const db = require('../config/dbConfig.js');
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/', (req, res) => {
+    res.send("Plants route");
+});
+
+router.get('/:id',  async (req, res) => {
+  try {
+      const {id} = req.params;
+      const plant = await db('plants').where({id}).first();
+      if(plant){
+          res.status(200).json(plant);
+      } else {
+          res.status(401).json({message: "The plant with provided id was not found in the data base"});
+      }
+
+  } catch(err) {
+      res.status(500).json({message: "There was en error trying to connect to the data base, please try again"});
+  }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const plantCount = await db('plants').where({id}).delete();
+        if(!plantCount || plantCount < 1){
+            res.status(404).json({message: "Plant was not found to be removed"})
+        } else{
+            res.status(200).json(plantCount);
+        }
+    }
+    catch (err) {
+        res.status(500).json({message: "There was an error while trying to delete a plant from the data base"});
+    }
+}); 
+
+
+router.put('/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const changes = req.body;
+        if(changes.name && changes.description) {
+            const updatedPlant = await db('plants').where({id}).update(changes);
+            if(updatedPlant) {
+                res.status(200).json(updatedPlant);
+            } else {
+                res.status(401).json({message: "The plant with provided id was not found"});
+            }
+        }
+    }
+    catch(err){
+        res.status(500).json({message: "There was an error while trying to update a plant in the data base"});
+    }
+});
+
+
+
+module.exports = router; 
---
+++ routes/usersUnprotected.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/dbConfig");
+
+// sanity check
+router.get('/', async (req, res) => {
+  const users = await db('users');
+  res.status(200).json(users);
+});
+
+router.get("/:id/plants", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const plants = await db('plants').where('userId', id);
+    res.status(200).json(plants);
+
+  } catch(err) {
+    res.status(500).json({message: "There was an error trying to connect to the data base, please try again"});
+  }
+});
+
+router.post('/:id/plants', async(req, res) => {
+  try {
+      const newPlant = req.body;
+      const id = req.params.id;
+      if(newPlant.name && newPlant.description) {
+          const addPlant = await db('plants').insert({...newPlant, userId: id});
+          if(addPlant) {
+              res.status(200).json(addPlant);
+          } else {
+              res.status(401).json({message: "The plant with provided id was not found"});
+          }
+      }
+  }
+  catch(err){
+      res.status(500).json({message: "There was an error while trying to add a plant in the data base"});
+  }
+});
+
+module.exports = router;
