f5502efc9d21d04fc41931687e4543c144618ce7
Lidii <lidiiany92@gmail.com>
2019-02-04 14:59:06 -0500

    Added unprotected routes

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ const userRoutes = require("../routes/userRoutes.js");
 const loginRoute = require('../routes/loginRoute.js');
 const registerRoute = require('../routes/registerRoute');
 const plantsRoute = require('../routes/plantsRoute');
+const usersUnpRoutes = require('../routes/usersUnprotected.js');
+const plantsUnpRoutes = require('../routes/plantsUprotected.js');
 
 // server
 const server = express();
@@ -16,6 +18,8 @@ server.use("/api/users/", userRoutes);
 server.use("/api/login/", loginRoute);
 server.use("/api/register/", registerRoute);
 server.use("/api/users/", userRoutes);
-server.use("/api/plants/", plantsRoute)
+server.use("/api/plants/", plantsRoute);
+server.use("/api/usersunp/", usersUnpRoutes);
+server.use("/api/plantsunp/", plantsUnpRoutes);
 
 module.exports = server;
