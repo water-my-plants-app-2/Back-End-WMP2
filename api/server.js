a207630949d6e7f1d4640b8d2c96c34c5a9bc29f
James Cassidy <cassidyjamesw@gmail.com>
2019-02-06 13:48:41 -0500

    added route to sever.js

--- api/server.js
+++ api/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const applyGlobalMiddleware = require("../config/globalMiddleware.js");
 const userRoutes = require("../routes/userRoutes.js");
-const loginRoute = require('../routes/loginRoute.js');
-const registerRoute = require('../routes/registerRoute');
-const plantsRoute = require('../routes/plantsRoute');
-const usersUnpRoutes = require('../routes/usersUnprotected.js');
-const plantsUnpRoutes = require('../routes/plantsUprotected.js');
+const loginRoute = require("../routes/loginRoute.js");
+const registerRoute = require("../routes/registerRoute");
+const plantsRoute = require("../routes/plantsRoute");
+const usersUnpRoutes = require("../routes/usersUnprotected.js");
+const plantsUnpRoutes = require("../routes/plantsUprotected.js");
+const appointmentsRoute = require("../routes/appointmentsRoute.js");
 
 // server
 const server = express();
@@ -21,5 +22,6 @@ server.use("/api/users/", userRoutes);
 server.use("/api/plants/", plantsRoute);
 server.use("/api/usersunp/", usersUnpRoutes);
 server.use("/api/plantsunp/", plantsUnpRoutes);
+server.use("/api/message/", appointmentsRoute);
 
 module.exports = server;
