eee2f0d3911b1564ad390999965414ce8844ae1a
James Cassidy <cassidyjamesw@gmail.com>
2019-02-06 16:16:22 -0500

    appointsroute.js error handling

--- routes/appointmentsRoute.js
+++ routes/appointmentsRoute.js
@@ -1,6 +1,7 @@
 const db = require("../config/dbConfig.js");
 const momentTimeZone = require("moment-timezone");
 const moment = require("moment");
+const schedule = require("node-schedule");
 const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
@@ -11,15 +12,26 @@ const client = twilio(
 
 router.get("/", (req, res) => {
   console.log("In message area\n");
+  try {
+    client.messages
+      .create({
+        from: process.env.TWILIO_PHONE_NUMBER,
+        body: "Water yer plants!!",
+        to: process.env.TWILIO_TARGET_NUMBER
+      })
+      .then(message => console.log(message.sid))
+      .done();
+    res.status(200);
+  } catch (err) {
+    res.status(500).json({
+      message:
+        "There was an error trying to connect to the data base, please try again"
+    });
+  }
+});
 
-  client.messages
-    .create({
-      from: process.env.TWILIO_PHONE_NUMBER,
-      body: "Water yer plants!!",
-      to: process.env.TWILIO_TARGET_NUMBER
-    })
-    .then(message => console.log(message.sid))
-    .done();
+router.post("/setschedule", async (req, res) => {
+  // const waterDate = req.body;
 });
 
 module.exports = router;
